0b59e36d841340c6d9658c00fdd79a9ff41071b8
Minhyuk <minpark815@gmail.com>
2017-11-08 17:24:32 -0600

    Add template for vecmat_multiply

--- src/facerec.c
+++ src/facerec.c
@@ -2,7 +2,23 @@
 #include <stdio.h>
 
 int main() {
-    double** mat = vecmat_multiply(NULL, NULL);
-    printf("%p", mat);
+    vector* vec = malloc(sizeof(size_t) + (5 * sizeof(double)));
+    vec->size = 5;
+    for(int i = 0; i < 5; i ++) {
+        vec->data[i] = i;
+    }
+
+    matrix* mat = malloc((2 * sizeof(size_t)) + (25 * sizeof(double)));
+    mat->row = 5;
+    mat->col = 5;
+
+    for(int i = 0; i < 25; i ++) {
+        mat->data[i] = i;
+    }
+    
+
+
+    double** mat_result = vecmat_multiply(vec, mat);
+    printf("%p", mat_result);
     return 0;
 }
