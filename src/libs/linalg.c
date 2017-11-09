ed7514408d52de53d6251d9e9d2243dc0c61e79c
TJ Egbejimba <tj98@live.com>
2017-11-08 19:58:09 -0600

    Add mat*vec and dotproduct function

--- src/libs/linalg.c
+++ src/libs/linalg.c
@@ -28,6 +28,24 @@ matrix* matrix_create(size_t row, size_t col) {
     retval->col = col;
     return retval;
 }
+/**
+ * @brief Performs dot product on two arrays
+ * this function will malloc for the user a double
+ * arrays must be of same size
+ * @return a double
+ * @param first array
+ * @param second array
+ * @param length of arrays
+ */
+double dot_product(const double* x, const double* y, int length)
+{
+    double retVal = 0.0;
+    for(int i =0;i<length; i++)
+    {
+        retVal+=x[i] *y[i];
+    }
+    return retVal;
+}
 
 /**
  * @brief Performs vector * matrix
@@ -44,4 +62,24 @@ matrix* matrix_create(size_t row, size_t col) {
 
     return NULL;
 }
- 
+
+
+ /**
+ * @brief Performs  matrix * vector
+ * this function will malloc for the user a vector*
+ * @return a vector*
+ * @param mat
+ * @param vec
+ */
+vector* matvec_multiply(const matrix* mat, const vector* vec)
+{
+    if(vec == NULL  || mat == NULL) {
+        return NULL;
+    }
+    vector *retVec = vector_create(vec->size);
+    for(size_t i =0; i< retVec->size; i++)
+    { 
+        VEC(retVec,i) = dot_product((double*)((mat->data) + mat->row*i), (double*)vec, vec->size);
+    }
+    return retVec;
+}
--- src/libs/linalg.h
+++ src/libs/linalg.h
@@ -27,7 +27,16 @@ vector* vector_create(size_t size);
 
 matrix* matrix_create(size_t row, size_t col);
 
-
+/**
+ * @brief Performs dot product on two arrays
+ * this function will malloc for the user a double
+ * arrays must be of same size
+ * @return a double
+ * @param first array
+ * @param second array
+ * @param length of arrays
+ */
+double dot_product(const double* x, const double* y, int length);
 
 /**
  * @brief Performs vector * matrix
@@ -41,4 +50,11 @@ matrix* matrix_create(size_t row, size_t col);
 
 
 
- 
+ /**
+ * @brief Performs  matrix * vector
+ * this function will malloc for the user a vector*
+ * @return a vector*
+ * @param mat
+ * @param vec
+ */
+vector* matvec_multiply(const matrix* mat, const vector* vec);
