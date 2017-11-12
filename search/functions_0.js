7b49175a9a31e862b2a3e4ec97751dbdd17ddfdd
Minhyuk <minpark815@gmail.com>
2017-11-11 20:44:12 -0600

    Update doxygen

---
+++ search/functions_0.js
@@ -0,0 +1,4 @@
+var searchData=
+[
+  ['covmat',['covmat',['../linalg_8c.html#ad41ec0281d3766f40963b77520ce600d',1,'covmat(matrix *mat):&#160;linalg.c'],['../linalg_8h.html#ad41ec0281d3766f40963b77520ce600d',1,'covmat(matrix *mat):&#160;linalg.c']]]
+];
