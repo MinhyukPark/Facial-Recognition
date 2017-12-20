5cc9d281d61a0b9e815d2d50bf9563d8c8a761c5
Minhyuk <minpark815@gmail.com>
2017-12-19 23:56:51 -0600

    Update docs

--- docs/html/search/all_2.js
+++ docs/html/search/all_2.js
@@ -2,5 +2,5 @@ var searchData=
 [
   ['data',['data',['../struct__vector.html#abcf7d1342126a90769b2a05126897859',1,'_vector::data()'],['../struct__matrix.html#ab0c92b382e007327a4cc5fc6818c77a9',1,'_matrix::data()']]],
   ['diag_5fvector',['diag_vector',['../linalg_8c.html#abc4918a0c6b295404ebfadfe78b26bdd',1,'diag_vector(int n, double a[], double v[]):&#160;linalg.c'],['../linalg_8h.html#abc4918a0c6b295404ebfadfe78b26bdd',1,'diag_vector(int n, double a[], double v[]):&#160;linalg.c']]],
-  ['dot_5fproduct',['dot_product',['../linalg_8c.html#abb2106d12022de4436ca036630b72c4a',1,'dot_product(const double *x, const double *y, int length):&#160;linalg.c'],['../linalg_8h.html#abb2106d12022de4436ca036630b72c4a',1,'dot_product(const double *x, const double *y, int length):&#160;linalg.c']]]
+  ['dot_5fproduct',['dot_product',['../linalg_8c.html#afc298e7d7d6a208e71ef073948b651d2',1,'dot_product(const double *left, const double *right, int length):&#160;linalg.c'],['../linalg_8h.html#afc298e7d7d6a208e71ef073948b651d2',1,'dot_product(const double *left, const double *right, int length):&#160;linalg.c']]]
 ];
--- docs/html/search/functions_1.js
+++ docs/html/search/functions_1.js
@@ -1,5 +1,5 @@
 var searchData=
 [
   ['diag_5fvector',['diag_vector',['../linalg_8c.html#abc4918a0c6b295404ebfadfe78b26bdd',1,'diag_vector(int n, double a[], double v[]):&#160;linalg.c'],['../linalg_8h.html#abc4918a0c6b295404ebfadfe78b26bdd',1,'diag_vector(int n, double a[], double v[]):&#160;linalg.c']]],
-  ['dot_5fproduct',['dot_product',['../linalg_8c.html#abb2106d12022de4436ca036630b72c4a',1,'dot_product(const double *x, const double *y, int length):&#160;linalg.c'],['../linalg_8h.html#abb2106d12022de4436ca036630b72c4a',1,'dot_product(const double *x, const double *y, int length):&#160;linalg.c']]]
+  ['dot_5fproduct',['dot_product',['../linalg_8c.html#afc298e7d7d6a208e71ef073948b651d2',1,'dot_product(const double *left, const double *right, int length):&#160;linalg.c'],['../linalg_8h.html#afc298e7d7d6a208e71ef073948b651d2',1,'dot_product(const double *left, const double *right, int length):&#160;linalg.c']]]
 ];
