628497d35c40e4a4915333322f249a088740a65f
Minhyuk <minpark815@gmail.com>
2017-12-19 23:58:33 -0600

    Update documentation

--- search/all_1.js
+++ search/all_1.js
@@ -1,5 +1,6 @@
 var searchData=
 [
   ['col',['col',['../struct__matrix.html#a7e9feb52cfc0a9d7cba5320abcaa997a',1,'_matrix']]],
+  ['compute_5faverage',['compute_average',['../linalg_8c.html#a71f490da601d057f4ce45efb9a48c2ae',1,'compute_average(matrix *images, int num_images):&#160;linalg.c'],['../linalg_8h.html#a71f490da601d057f4ce45efb9a48c2ae',1,'compute_average(matrix *images, int num_images):&#160;linalg.c']]],
   ['covmat',['covmat',['../linalg_8c.html#ad41ec0281d3766f40963b77520ce600d',1,'covmat(matrix *mat):&#160;linalg.c'],['../linalg_8h.html#ad41ec0281d3766f40963b77520ce600d',1,'covmat(matrix *mat):&#160;linalg.c']]]
 ];
--- search/functions_0.js
+++ search/functions_0.js
@@ -1,4 +1,5 @@
 var searchData=
 [
+  ['compute_5faverage',['compute_average',['../linalg_8c.html#a71f490da601d057f4ce45efb9a48c2ae',1,'compute_average(matrix *images, int num_images):&#160;linalg.c'],['../linalg_8h.html#a71f490da601d057f4ce45efb9a48c2ae',1,'compute_average(matrix *images, int num_images):&#160;linalg.c']]],
   ['covmat',['covmat',['../linalg_8c.html#ad41ec0281d3766f40963b77520ce600d',1,'covmat(matrix *mat):&#160;linalg.c'],['../linalg_8h.html#ad41ec0281d3766f40963b77520ce600d',1,'covmat(matrix *mat):&#160;linalg.c']]]
 ];
